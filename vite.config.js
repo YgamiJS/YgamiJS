c0f91a790d205577fed8b5f6473eb2b9fcd575aa
Дмитрий <105663726+YgamiJS@users.noreply.github.com>
2023-05-23 18:43:20 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import viteImagemin from "vite-plugin-imagemin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/YgamiJS/,
   plugins: [
     viteImagemin({
       gifsicle: {
