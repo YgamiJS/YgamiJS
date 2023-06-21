d989ec051ced650b847bcb3277aac06575cd84cf
Dima <dimasakharov@icloud.com>
2023-06-21 12:01:36 +0300

    add work

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import viteImagemin from "vite-plugin-imagemin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/YgamiJS/,
+  base: "/YgamiJS/",
   plugins: [
     viteImagemin({
       gifsicle: {
